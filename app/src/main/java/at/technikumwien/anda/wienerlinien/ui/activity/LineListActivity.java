aa55c9f01bcf143c1c4111d212472dc8bafd0f47
David Schreiber <david@blockhaus-medien.at>
2015-03-24 15:01:08 +0100

    feat(lines): use master-detail flow for displaying list of lines and line details

--- app/src/main/java/at/technikumwien/anda/wienerlinien/ui/activity/LineListActivity.java
+++ app/src/main/java/at/technikumwien/anda/wienerlinien/ui/activity/LineListActivity.java
@@ -1,142 +1,83 @@
 package at.technikumwien.anda.wienerlinien.ui.activity;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import at.technikumwien.anda.wienerlinien.R;
-import at.technikumwien.anda.wienerlinien.model.Line;
 
+/**
+ * An activity representing a list of Lines. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link LineDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p/>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link LineListFragment} and the item details
+ * (if present) is a {@link LineDetailFragment}.
+ * <p/>
+ * This activity also implements the required
+ * {@link LineListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class LineListActivity extends FragmentActivity
+    implements LineListFragment.Callbacks {
 
-public class LineListActivity extends ActionBarActivity {
-
-    // =============================================================================
-    // Views
-    // =============================================================================
-
-    // =============================================================================
-    // Supertype overrides
-    // =============================================================================
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_list);
 
-        // Extract ListView from layout
-        ListView listView = (ListView) findViewById(R.id.listView);
+        if (findViewById(R.id.line_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
 
-        // Create hard-coded list of lines
-        ArrayList<Line> lines = new ArrayList<>();
-        lines.add(new Line("U1", Color.parseColor("#E20A16")));
-        lines.add(new Line("U2", Color.parseColor("#764785")));
-        lines.add(new Line("U3", Color.parseColor("#F76013")));
-        lines.add(new Line("U4", Color.parseColor("#008131")));
-        lines.add(new Line("U6", Color.parseColor("#88471F")));
-        lines.add(new Line("31", Color.parseColor("#999999")));
-
-        // Create an set adapter for displaying the lines
-        LineAdapter adapter = new LineAdapter(lines);
-        listView.setAdapter(adapter);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_line_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((LineListFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.line_list))
+                .setActivateOnItemClick(true);
         }
 
-        return super.onOptionsItemSelected(item);
+        // TODO: If exposing deep links into your app, handle intents here.
     }
-
-    // =============================================================================
-    // Inner classes
-    // =============================================================================
 
     /**
-     * Simple adapter for displaying a list of lines
+     * Callback method from {@link LineListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
      */
-    public static class LineAdapter extends BaseAdapter {
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(LineDetailFragment.ARG_ITEM_ID, id);
+            LineDetailFragment fragment = new LineDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.line_detail_container, fragment)
+                .commit();
 
-        // =============================================================================
-        // Private members
-        // =============================================================================
-
-        private List<Line> lines;
-
-        // =============================================================================
-        // Constructor
-        // =============================================================================
-
-        public LineAdapter(List<Line> lines) {
-            if (lines == null) {
-                throw new NullPointerException("lines has to be an initialized List object");
-            }
-
-            this.lines = lines;
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, LineDetailActivity.class);
+            detailIntent.putExtra(LineDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
         }
-
-        // =============================================================================
-        // Supertype overrides
-        // =============================================================================
-
-        @Override public int getCount() {
-            return lines.size();
-        }
-
-        @Override public Line getItem(int position) {
-            return lines.get(position);
-        }
-
-        @Override public long getItemId(int position) {
-            return position;
-        }
-
-        @Override public View getView(int position, View convertView, ViewGroup parent) {
-            TextView view = (TextView) convertView;
-
-            // If no convert view was provided create a new view by inflation
-            if (view == null) {
-                view = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_line, parent, false);
-            }
-
-            final Line line = getItem(position);
-
-            // Set line name
-            view.setText(line.getName());
-
-            // set line color
-            view.setBackground(new ColorDrawable(line.getColor()));
-
-            return view;
-        }
-
     }
-
 }
---
+++ app/src/main/java/at/technikumwien/anda/wienerlinien/ui/activity/LineListFragment.java
@@ -0,0 +1,228 @@
+package at.technikumwien.anda.wienerlinien.ui.activity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import at.technikumwien.anda.wienerlinien.R;
+import at.technikumwien.anda.wienerlinien.model.Line;
+import at.technikumwien.anda.wienerlinien.ui.activity.dummy.DummyContent;
+
+/**
+ * A list fragment representing a list of Lines. This fragment
+ * also supports tablet devices by allowing list items to be given an
+ * 'activated' state upon selection. This helps indicate which item is
+ * currently being viewed in a {@link LineDetailFragment}.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link Callbacks}
+ * interface.
+ */
+public class LineListFragment extends ListFragment {
+
+    /**
+     * The serialization (saved instance state) Bundle key representing the
+     * activated item position. Only used on tablets.
+     */
+    private static final String STATE_ACTIVATED_POSITION = "activated_position";
+
+    /**
+     * The fragment's current callback object, which is notified of list item
+     * clicks.
+     */
+    private Callbacks mCallbacks = sDummyCallbacks;
+
+    /**
+     * The current activated item position. Only used on tablets.
+     */
+    private int mActivatedPosition = ListView.INVALID_POSITION;
+
+    /**
+     * A callback interface that all activities containing this fragment must
+     * implement. This mechanism allows activities to be notified of item
+     * selections.
+     */
+    public interface Callbacks {
+        /**
+         * Callback for when an item has been selected.
+         */
+        public void onItemSelected(String id);
+    }
+
+    /**
+     * A dummy implementation of the {@link Callbacks} interface that does
+     * nothing. Used only when this fragment is not attached to an activity.
+     */
+    private static Callbacks sDummyCallbacks = new Callbacks() {
+        @Override
+        public void onItemSelected(String id) {
+        }
+    };
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public LineListFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create hard-coded list of lines
+
+
+        // Create an set adapter for displaying the lines
+        LineAdapter adapter = new LineAdapter(lines);
+
+        // Provide list view with line adapter
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Restore the previously serialized activated item position.
+        if (savedInstanceState != null
+            && savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
+            setActivatedPosition(savedInstanceState.getInt(STATE_ACTIVATED_POSITION));
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // Activities containing this fragment must implement its callbacks.
+        if (!(activity instanceof Callbacks)) {
+            throw new IllegalStateException("Activity must implement fragment's callbacks.");
+        }
+
+        mCallbacks = (Callbacks) activity;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        // Reset the active callbacks interface to the dummy implementation.
+        mCallbacks = sDummyCallbacks;
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View view, int position, long id) {
+        super.onListItemClick(listView, view, position, id);
+
+        // Notify the active callbacks interface (the activity, if the
+        // fragment is attached to one) that an item has been selected.
+        mCallbacks.onItemSelected(DummyContent.ITEMS.get(position).id);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mActivatedPosition != ListView.INVALID_POSITION) {
+            // Serialize and persist the activated item position.
+            outState.putInt(STATE_ACTIVATED_POSITION, mActivatedPosition);
+        }
+    }
+
+    /**
+     * Turns on activate-on-click mode. When this mode is on, list items will be
+     * given the 'activated' state when touched.
+     */
+    public void setActivateOnItemClick(boolean activateOnItemClick) {
+        // When setting CHOICE_MODE_SINGLE, ListView will automatically
+        // give items the 'activated' state when touched.
+        getListView().setChoiceMode(activateOnItemClick
+            ? ListView.CHOICE_MODE_SINGLE
+            : ListView.CHOICE_MODE_NONE);
+    }
+
+    private void setActivatedPosition(int position) {
+        if (position == ListView.INVALID_POSITION) {
+            getListView().setItemChecked(mActivatedPosition, false);
+        } else {
+            getListView().setItemChecked(position, true);
+        }
+
+        mActivatedPosition = position;
+    }
+
+    /**
+     * Simple adapter for displaying a list of lines
+     */
+    public static class LineAdapter extends BaseAdapter {
+
+        // =============================================================================
+        // Private members
+        // =============================================================================
+
+        private List<Line> lines;
+
+        // =============================================================================
+        // Constructor
+        // =============================================================================
+
+        public LineAdapter(List<Line> lines) {
+            if (lines == null) {
+                throw new NullPointerException("lines has to be an initialized List object");
+            }
+
+            this.lines = lines;
+        }
+
+        // =============================================================================
+        // Supertype overrides
+        // =============================================================================
+
+        @Override public int getCount() {
+            return lines.size();
+        }
+
+        @Override public Line getItem(int position) {
+            return lines.get(position);
+        }
+
+        @Override public long getItemId(int position) {
+            return position;
+        }
+
+        @Override public View getView(int position, View convertView, ViewGroup parent) {
+            TextView view = (TextView) convertView;
+
+            // If no convert view was provided create a new view by inflation
+            if (view == null) {
+                view = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_line, parent, false);
+            }
+
+            final Line line = getItem(position);
+
+            // Set line name
+            view.setText(line.getName());
+
+            // set line color
+            if(Build.VERSION.SDK_INT >= 16) {
+                view.setBackground(new ColorDrawable(line.getColor()));
+            } else {
+                view.setBackgroundDrawable(new ColorDrawable(line.getColor()));
+            }
+
+            return view;
+        }
+
+    }
+}
